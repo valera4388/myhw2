aa88aab9c53474d2a0c545c1ea9761ee2b94a091
mikhalev valera <mihalev.valera@mail.ru>
2024-08-11 15:51:05 +0300

    add prog commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balance = 100; //баланс на счете
+        int deposit = 5000; //сумма вносимая нас счет
+
+        int bonus = deposit / 100;
+        if (deposit < 1000) {
+            bonus = 0;
+        }
+
+        System.out.println("Количество начисленных бонусов: " + bonus);
+        int balanceСheck = balance + deposit + bonus;
+        System.out.println("Итоговый баланс на счете: " + balanceСheck);
     }
 }
